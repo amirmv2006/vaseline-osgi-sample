6d78d75f9a103a0d6579568e6e7167cf13a93e9d
Amir <amirmv2006@gmail.com>
2018-01-26 00:50:14 +0800

    multiparam rest

--- ir.amv.os.vaseline.samples.book.rest/src/main/java/ir/amv/os/vaseline/samples/book/rest/IBookRestService.java
+++ ir.amv.os.vaseline.samples.book.rest/src/main/java/ir/amv/os/vaseline/samples/book/rest/IBookRestService.java
@@ -1,11 +1,20 @@
 package ir.amv.os.vaseline.samples.book.rest;
 
+import ir.amv.os.vaseline.basics.apis.core.shared.base.dto.paging.PagingDto;
+import ir.amv.os.vaseline.basics.apis.core.shared.base.exc.BaseVaselineClientException;
 import ir.amv.os.vaseline.samples.books.model.shared.BookDto;
+import ir.amv.os.vaseline.ws.rest.apis.basic.layer.base.multiparam.annot.JsonMultParam;
+import ir.amv.os.vaseline.ws.rest.apis.basic.layer.base.multiparam.annot.JsonParam;
 import ir.amv.os.vaseline.ws.rest.apis.basic.layer.crud.IBaseCrudRestService;
 import ir.amv.os.vaseline.ws.rest.apis.basic.layer.ro.IBaseReadOnlyRestService;
 import ir.amv.os.vaseline.ws.rest.apis.simplesearch.layer.IBaseSimpleSearchRestService;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Amir
@@ -14,4 +23,11 @@ import javax.ws.rs.Path;
 public interface IBookRestService extends
         IBaseCrudRestService<BookDto, Long>, IBaseSimpleSearchRestService<BookDto, Long> {
 
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/query/page")
+    List<BookDto> searchByExamplePaged(@JsonMultParam({
+            @JsonParam(paramName = "example", paramType = BookDto.class),
+            @JsonParam(paramName = "pagingDto", paramType = PagingDto.class)
+    }) Map<String, Object> objectMap) throws BaseVaselineClientException;
 }
--- ir.amv.os.vaseline.samples.book.restimpl/src/main/java/ir/amv/os/vaseline/samples/book/restimpl/BookRestServiceImpl.java
+++ ir.amv.os.vaseline.samples.book.restimpl/src/main/java/ir/amv/os/vaseline/samples/book/restimpl/BookRestServiceImpl.java
@@ -1,5 +1,7 @@
 package ir.amv.os.vaseline.samples.book.restimpl;
 
+import ir.amv.os.vaseline.basics.apis.core.shared.base.dto.paging.PagingDto;
+import ir.amv.os.vaseline.basics.apis.core.shared.base.exc.BaseVaselineClientException;
 import ir.amv.os.vaseline.samples.book.rest.IBookRestService;
 import ir.amv.os.vaseline.samples.book.service.api.IBookServiceApi;
 import ir.amv.os.vaseline.samples.books.model.shared.BookDto;
@@ -8,6 +10,9 @@ import ir.amv.os.vaseline.ws.rest.apis.basic.layerimpl.crud.IBaseImplementedCrud
 import ir.amv.os.vaseline.ws.rest.apis.simplesearch.layerimpl.IBaseImplementedSimpleSearchRestService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Amir
@@ -30,5 +35,11 @@ public class BookRestServiceImpl
     @Reference
     public void setBookService(final IBookServiceApi bookService) {
         this.bookService = bookService;
+    }
+
+    @Override
+    public List<BookDto> searchByExamplePaged(final Map<String, Object> objectMap) throws BaseVaselineClientException {
+        return IBaseImplementedSimpleSearchRestService.super.searchByExample((BookDto)objectMap.get("example"),
+                (PagingDto)objectMap.get("pagingDto"));
     }
 }
